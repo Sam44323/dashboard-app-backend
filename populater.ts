62e6e585a7aebba12f1add4dffe0bb5e7990f5f6
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-09 00:36:33 +0530

    update: updated the populater

--- populater.ts
+++ populater.ts
@@ -1,50 +1,12 @@
 import {
-  CollegeDescription,
-  CollegeDescriptionModel,
+  StudentDescription,
   StudentDescriptionModel,
 } from "./model/dashboard.model";
 
-let [c, s] = [1, 1];
-let state = ["WB", "AP", "GJ", "RJ", "JK", "SK", "NG", "KL"];
-let city = ["City 1", "City 2", "City 3", "City 4", "City"];
-let courses = ["CS", "BCOM", "BBA", "MPhiL", "DPhil", "MSC", "MBA", "MCOM"];
-let country = ["India", "USA", "UK", "Canada", "Australia"];
-let years = [1941, 1942, 1943, 1944, 1981, 1982, 1983, 2001, 2002, 2003, 2004];
-let skills = [
-  "Coding",
-  "Script Writing",
-  "Film-making",
-  "Accounts",
-  "Photography",
-  "Singing",
-];
+let s = 1;
+
+let years = [1978, 1979, 1980, 1981, 1985, 1986, 1987, 1988, 2001];
 
 export const populate = async () => {
-  let college: CollegeDescription = new CollegeDescriptionModel();
-  college.college_name = `College ${c++}`.toString();
-  college.state_name = state[Math.floor(Math.random() * state.length)];
-  college.founded_on = years[Math.floor(Math.random() * years.length)];
-  college.city_name = city[Math.floor(Math.random() * city.length)];
-  college.no_students = Math.floor(Math.random() * 1000);
-  college.country_name = country[Math.floor(Math.random() * country.length)];
-  college.courses_offered = courses.slice(
-    0,
-    courses.length - Math.floor(Math.random() * courses.length - 1)
-  );
-
-  console.log(college);
-
-  CollegeDescriptionModel.create(college, (err, res) => {
-    if (err) {
-      console.log(err);
-    }
-  });
-  console.log("create-another");
+  let studentDescription: StudentDescription = new StudentDescriptionModel();
 };
-
-export const fetcherIds = async () =>
-  await (
-    await CollegeDescriptionModel.find({}, { _id: 1 })
-  ).map((doc) => {
-    return doc._id.toString();
-  });
--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/connectDB";
 import logger from "./utils/logger";
-import { fetcherIds, populate } from "./populater";
+import { populate } from "./populater";
 dotenv.config({
   path: ".env",
 });
@@ -16,9 +16,6 @@ app.use(express.json());
 connectDB();
 
 const port = process.env.PORT || 5000;
-app.listen(port, async () => {
+app.listen(port, () => {
   logger.info(`Server started on port ${port}`);
-  console.log("populating");
-  const data = await fetcherIds();
-  console.log(data);
 });
