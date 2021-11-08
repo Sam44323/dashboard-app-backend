1569a36ec9b028a6220e82c83db40e8ecb69e734
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-08 22:26:32 +0530

    add: added initial server build and update the scripts

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
 // write codes to create a server with express using typescript
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
