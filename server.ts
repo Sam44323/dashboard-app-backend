84c9e9a86399eaa6d496338602c045a5252faca8
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-08 22:35:51 +0530

    add: added files for utilities

--- server.ts
+++ server.ts
@@ -1,11 +1,16 @@
 // write codes to create a server with express using typescript
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({
+  path: ".env",
+});
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+console.log(process.env.MONGO_URL);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
