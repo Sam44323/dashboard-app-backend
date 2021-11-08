74cd1f16d13f2cf80e2c11a43ea3d118bde67d74
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-08 22:46:50 +0530

    add: added database connector utils

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./utils/connectDB";
 dotenv.config({
   path: ".env",
 });
@@ -10,7 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-console.log(process.env.MONGO_URL);
+connectDB();
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- utils/connectDB.ts
+++ utils/connectDB.ts
@@ -1,3 +1,14 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
-const connectDB = async () => {};
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL as string);
+    console.log("connected to DB successfully");
+  } catch (err: any) {
+    console.log("couldn't connect to database:", err.message);
+  }
+};
+
+export default connectDB;
