80931dde12c7105201bcdfa4afafdbf6bef615c2
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-09 12:51:20 +0530

    update: updated the route for college

--- routes/college.route.ts
+++ routes/college.route.ts
@@ -2,5 +2,5 @@ import { Router } from "express";
 import { getColleges } from "../controllers/college.controller";
 
 export const router = Router();
-router.get("/college/:name");
+router.get("/college");
 router.get("/colleges", getColleges);
